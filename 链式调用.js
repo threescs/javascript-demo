e58826c5bb7ce93b173b95241d3c72f09dd585e6
threescs <95513113@qq.com>
2018-09-25 15:20:16 +0800

    链式调用
    链式调用

---
+++ 链式调用.js
@@ -0,0 +1,23 @@
+class Utils {
+	constructor(value = 0){
+		this.value = value
+	}
+	add(...attr){
+		const data = [...attr,this.value].reduce((num,v) => {
+			num += v
+			return num
+		},0)
+		this.value = data
+		return this
+	}
+	valueOf(){
+		return this.value
+	}
+	toString(){
+		return this.value.toString()
+	}
+}
+
+const u = new Utils(2);
+
+alert(u.add(1,2,3).add(4),add(5))
